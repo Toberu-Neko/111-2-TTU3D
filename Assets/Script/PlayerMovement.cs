5c823525e506f47d0a5c490232ffdc52385891fb
u10914226 <u10914226@ms.ttu.edu.tw>
2023-02-21 12:14:28 +0800

    Player movement script, input system testing.

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,17 +4,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
+    Vector3 desireDirection;
     [SerializeField] private float moveSpeed;
-
-    void Update()
+    [SerializeField] private LayerMask ground;
+    private RaycastHit groundHit;
+    private Transform groundDetector;
+    private bool isGrounded;
+    private void Start()
+    {
+        groundDetector = transform.Find("Misc/GroundDetector");
+        isGrounded = Physics.Raycast(groundDetector.position, Vector3.down, .3f, ground);
+        desireDirection = Vector3.forward;
+    }
+    private void FixedUpdate()
     {
         if (PlayerStatus.moveable)
         {
             Movement();
         }
     }
+    void Update()
+    {
+        isGrounded = Physics.Raycast(groundDetector.position, Vector3.down, .1f, ground);
+
+    }
     void Movement()
     {
-        transform.Translate(moveSpeed * Time.deltaTime * transform.forward);
+        if (Physics.Raycast(groundDetector.position, Vector3.down, out groundHit, .1f, ground))
+        {
+            desireDirection = Vector3.ProjectOnPlane(Vector3.forward, groundHit.normal);
+        }
+        transform.Translate(moveSpeed * Time.deltaTime * desireDirection);
     }
 }
